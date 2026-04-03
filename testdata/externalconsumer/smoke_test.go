09e082f43a70c1ec44df7adb75ab53fcd4b180fa
tuan.vm <tuan.uet@gmail.com>
2026-04-03 11:41:30 +0700

    chore: fix gofmt formatting in external consumer smoke test

--- testdata/externalconsumer/smoke_test.go
+++ testdata/externalconsumer/smoke_test.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/tuanuet/lockman"
 	backendredis "github.com/tuanuet/lockman/backend/redis"
-	idempotencyredis "github.com/tuanuet/lockman/idempotency/redis"
 	guardpostgres "github.com/tuanuet/lockman/guard/postgres"
+	idempotencyredis "github.com/tuanuet/lockman/idempotency/redis"
 )
 
 func TestReleasedModulesCompileTogether(t *testing.T) {
